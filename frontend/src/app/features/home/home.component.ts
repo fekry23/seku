2a69df528f0e4ebf09856c7ab1d8c816b7de0f3d
Fekry <fekrysaharudins@gmail.com>
2025-03-27 21:40:51 +0800

    feat: add buttons for user navigation in home page header

--- frontend/src/app/features/home/home.component.ts
+++ frontend/src/app/features/home/home.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  isMobile: boolean = false;
+
+  constructor(private responsive: BreakpointObserver) {
+
+  }
+
+  ngOnInit() {
+    this.responsive.observe([Breakpoints.Handset]).subscribe(result => {
+      this.isMobile = result.matches;
+    });
+  }
 
 }
